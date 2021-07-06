b6ddd2bd2bacdd9b1e438cb95dfdcbe55f0f3524
Carson C <carson.carraher@gmail.com>
2021-07-05 19:24:58 -0700

    created server.js and got everything to display on page

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const notes = require('./db/db.json');
+const note = JSON.parse(fs.readFileSync('./db/db.json'));
+
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+app.get('/api/notes', (req, res) => res.json(note));
+
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
