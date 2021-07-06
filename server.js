f608d0af32c668fc4e94e5a49ccbb2fec667bd4a
Carson C <carson.carraher@gmail.com>
2021-07-05 19:36:07 -0700

    finished post and delete routes

--- server.js
+++ server.js
@@ -19,5 +19,27 @@ app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes
 
 app.get('/api/notes', (req, res) => res.json(note));
 
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    note.push(newNote);
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(note), err => {
+        if (err)
+        throw (err);
+      });  
+      res.json(newNote);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    const deleteNote = note
+    .findIndex(i => i.id == req.params.id);  
+    note.splice(deleteNote, 1);
+  
+    fs.writeFileSync('./db/db.json', JSON.stringify(note), err => {
+      if (err)
+        throw (err);
+    });
+    return res.json(true);
+  });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
